464b3ab8fd96a2ef7e4071c37fef5fd1af21c568
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-01-14 23:59:06 +0000

    added bottom elements (menu)

--- components/elements/Menu.js
+++ components/elements/Menu.js
@@ -19,33 +19,54 @@ export default function Menu() {
         onClick={collapseMenu}
         className="h-screen w-screen absolute z-40 bg-muted opacity-50 backdrop-blur-md -translate-x-full"
       ></div>
+
       <div
         id="menu"
-        className="h-screen w-3/4 absolute z-50 bg-white pt-1 -translate-x-full"
+        className="h-screen w-3/4 absolute z-50 bg-white pt-1 -translate-x-full flex flex-col justify-between"
       >
         <div>
-          <img src="/img/logo_alt.svg" className="mt-6 ml-6"></img>
+          <div className="">
+            <img src="/img/logo_alt.svg" className="mt-6 ml-6"></img>
+          </div>
+          <MenuList>
+            <li className="selected">
+              <img src="/img/home-icon.svg" className="ml-6"></img>
+              <h2>Homepage</h2>
+            </li>
+            <li className="">
+              <img src="/img/chart-pie-icon.svg" className="ml-6"></img>
+              <h2>Histórico uso</h2>
+            </li>
+            <li className="">
+              <img src="/img/check-circle-icon.svg" className="ml-6"></img>
+              <h2>Tarefas</h2>
+            </li>
+            <li className="">
+              <img src="/img/sun-icon.svg" className="ml-6"></img>
+              <h2>Rotinas</h2>
+            </li>
+            <li className="">
+              <img src="/img/currency-dollar-icon.svg" className="ml-6"></img>
+              <h2>Pagamentos</h2>
+            </li>
+          </MenuList>
         </div>
-        <MenuList>
-          <li className="selected">
-            <img src="/img/home-icon.svg" className="ml-6"></img>
-            <h2>Homepage</h2>
+
+        <MenuList size="sm" className="mb-6">
+          <li className="h-8">
+            <img src="/img/user-bird.svg" className="ml-6"></img>
+            <div>
+              <h4>Pedro</h4>
+              <p>Editar perfil</p>
+            </div>
           </li>
-          <li className="">
-            <img src="/img/chart-pie-icon.svg" className="ml-6"></img>
-            <h2>Histórico uso</h2>
+          <li className="h-8">
+            <img src="/img/icon-settings.svg" className="ml-6"></img>
+            <h4>Definições</h4>
           </li>
-          <li className="">
-            <img src="/img/check-circle-icon.svg" className="ml-6"></img>
-            <h2>Tarefas</h2>
-          </li>
-          <li className="">
-            <img src="/img/sun-icon.svg" className="ml-6"></img>
-            <h2>Rotinas</h2>
-          </li>
-          <li className="">
-            <img src="/img/currency-dollar-icon.svg" className="ml-6"></img>
-            <h2>Pagamentos</h2>
+          <li className="h-8">
+            <img src="/img/icon-help-circle.svg" className="ml-6"></img>
+            <h4>Ajuda</h4>
           </li>
         </MenuList>
       </div>
@@ -87,6 +108,26 @@ const MenuList = styled("ul", {
   "li:nth-child(5)": {
     "&:hover, &.selected": {
       backgroundColor: "$skyblue",
+    },
+  },
+  variants: {
+    size: {
+      sm: {
+        li: {
+          display: "flex",
+          gap: "1.5rem",
+          height: "3.5rem",
+          alignItems: "center",
+          borderRadius: "0px 8px 8px 0px",
+          width: "90%",
+          backgroundColor: "transparent",
+        },
+        "li:nth-child(n)": {
+          "&:hover, &.selected": {
+            backgroundColor: "transparent",
+          },
+        },
+      },
     },
   },
 });
