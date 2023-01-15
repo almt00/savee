c848699f9e96bf842b0d3df6f664772ce8f04739
= <marianaalves20@ua.pt>
2023-01-15 17:12:47 +0000

    [WIP] trying chart attempt

--- components/elements/Chart.js
+++ components/elements/Chart.js
@@ -1,18 +1,34 @@
-import { styled } from '@stitches/react';
-import useSWR from 'swr';
-import Card from './Card';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS } from  'chart.js/auto';
+// import { styled } from '@stitches/react';
+// import useSWR from 'swr';
+// import Card from './Card';
+// import { Doughnut } from 'react-chartjs-2';
+// import { Chart as ChartJS } from  'chart.js/auto';
 
-const fetcher = url =>
-  fetch(url)
-    .then(res => res.json())
-    .then(res => JSON.parse(res));
+// const fetcher = url =>
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(res => JSON.parse(res));
 
 
-function Chart({ chartData }) {
-  return <Doughnut 
-        data={chartData}
-      />
-}
-export default Chart; 
+// function Chart() {
+//   let totalValue = 55;
+
+//   const { data, error } = useSWR('/api/user_1', fetcher);
+
+//   //Handle the error state
+//   if (error) return <div>Failed to load</div>;
+//   //Handle the loading state
+//   if (!data) return <div>Loading...</div>;
+
+//   if (data) {
+//     console.log(typeof data.hist_payment);
+//     console.log(data.hist_payment);
+//     // data.hist_payment.forEach(element => {
+//     //   totalValue = element.total_value;
+//     //   monthPercentage = element.percentage;
+//     // });
+//   }
+
+//   return <Doughnut data={totalValue}/>
+// }
+// export default Chart; 
