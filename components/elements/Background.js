033a526d5176fbe192e71c2f14a92f3dffd46392
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-01-15 14:37:01 +0000

    button centered, bg fixed

--- components/elements/Background.js
+++ components/elements/Background.js
@@ -5,8 +5,8 @@ const Background = (props) => {
 };
 
 const Container = styled("div", {
-  background:
-    "linear-gradient(180deg, $mint 0%, $mint 35%, $mintTransparent 35%, $white 100%);",
+  /* background:
+    "linear-gradient(180deg, $mint 0%, $mint 35%, $mintTransparent 35%, $white 100%);", */
   zIndex: "-1",
   borderRadius: "0 0 12px 12px",
 });
