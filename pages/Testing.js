d6a69b4d347f16b072a7ff27a015b28be652e27f
amieUA <6574688+heyamie@users.noreply.github.com>
2023-01-16 18:49:48 +0000

    Selector dias semana

--- pages/Testing.js
+++ pages/Testing.js
@@ -4,6 +4,7 @@ import Header from "../components/elements/Header";
 import Card from "../components/elements/Card";
 import Form from "../components/elements/Form";
 import Tasks from "../components/dashboard/Tasks";
+import Selector from "../components/routines/Selector";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,9 @@ export default function Home() {
         <Form type="date" />
       </Card>
       <Tasks />
+
+      <Selector />
+
     </>
   );
 }
