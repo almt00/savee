d8ef00e9deee1d2fa178a13335e82e9710549ee6
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-01-17 18:40:48 +0000

    cleaned up files

--- components/routines/TimeSelector.js
+++ components/routines/TimeSelector.js
@@ -1,55 +1,49 @@
 import { styled } from "@stitches/react";
 import React from "react";
-import Button from "../elements/Button";
-
 
 export default function TimeSelector() {
   return (
     <>
-        <input
-          type="time"
-          id="time"
-          min="00:01:00"
-          max="24:00:00"
-          className="w-full border-0"
-          step={1}
-          required
-        ></input>
-      <div className={`flex gap-6 justify-center text-center p-3 mt-6`}>
-        <div className="border-b-2 border-purple">
-          <p className="text-muted">hr</p>
-          <Input type="number" placeholder="00" maxLength="2" min={0} max={24} />
-        </div>
-        <div className="border-b-2 border-purple">
-          <p className="text-muted">min</p>
-          <h1>00</h1>
-        </div>
-        <div className="border-b-2 border-purple">
-          <p className="text-muted">sec</p>
-          <h1>00</h1>
-        </div>
-      </div>
       <form className={`flex gap-6 justify-center text-center p-3 mt-6`}>
         <div className="border-b-2 border-purple">
           <p className="text-muted">hr</p>
-          <Input type="number" placeholder="00" maxLength="2" min={0} max={24} />
+          <Input
+            type="number"
+            placeholder="00"
+            maxLength="2"
+            min={0}
+            max={24}
+          />
         </div>
         <div className="border-b-2 border-purple">
           <p className="text-muted">min</p>
-          <Input type="number" placeholder="00" maxLength="2" min={0} max={60} required/>
+          <Input
+            type="number"
+            placeholder="00"
+            maxLength="2"
+            min={0}
+            max={60}
+            required
+          />
         </div>
         <div className="border-b-2 border-purple">
           <p className="text-muted">sec</p>
-          <Input type="number" placeholder="00" maxLength="2" min={0} max={60} required/>
+          <Input
+            type="number"
+            placeholder="00"
+            maxLength="2"
+            min={0}
+            max={60}
+            required
+          />
         </div>
-        <Button type="submit" >teste</Button>
       </form>
     </>
   );
 }
 
 const Input = styled("input", {
-    "-webkit-appearance": "none",
+  "-webkit-appearance": "none",
   maxWidth: "3rem",
   height: "3rem",
   fontSize: "$h1",
