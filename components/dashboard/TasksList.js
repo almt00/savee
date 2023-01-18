99dc10912475dde46eb5d3ff3bdd3e8dde7a48dc
edgar <edgar.quintero@ua.pt>
2023-01-18 08:13:55 +0000

    state hook to filter items inside every content

--- components/dashboard/TasksList.js
+++ components/dashboard/TasksList.js
@@ -1,4 +1,5 @@
 import { styled } from "@stitches/react";
+import { useState } from "react";
 import Entries from "./Entries";
 import React from 'react';
 import * as Accordion from '@radix-ui/react-accordion';
@@ -6,43 +7,58 @@ import * as Accordion from '@radix-ui/react-accordion';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 
 
-const TasksList = () => (
+const TasksList = () => {
+
+const [dayMoment, setDayMoment] = useState("morning");
+
+const morning =()=>{
+    setDayMoment("morning");
+}
+const noon =()=>{
+    setDayMoment("noon");
+}
+const afternoon =()=>{
+    setDayMoment("afternoon");
+}
+ 
+    //Root box display 3 item boxes, one for each part of the day, morning, noon, afternoon.
+    //To be able to filter the items presented on the list, props must be pass on inside the Entries tag
     <Accordion.Root style={root} type="single" defaultValue="item-1" collapsible>
         <Accordion.Item style={item} value="item-1">
             <Accordion.Header style={header}>
                  <h5>Manhã</h5>
-                <Accordion.Trigger style={trigger}>
+                <Accordion.Trigger style={trigger} onClick={morning}>
                     <ChevronDownIcon style={chevron} aria-hidden />
                 </Accordion.Trigger>
             </Accordion.Header>
             <Accordion.Content>
-                <Entries />
+                <Entries props={dayMoment}/>
             </Accordion.Content>
         </Accordion.Item>
         <Accordion.Item  style={item} value="item-2">
             <Accordion.Header style={header}>
                 <h5>Tarde</h5>
-                <Accordion.Trigger style={trigger}>
+                <Accordion.Trigger style={trigger}  onClick={noon}>
                     <ChevronDownIcon style={chevron} aria-hidden />
                 </Accordion.Trigger>
             </Accordion.Header>
             <Accordion.Content>
-                <Entries />
+                <Entries props={dayMoment}/>
             </Accordion.Content>
         </Accordion.Item>
         <Accordion.Item  style={item} value="item-3">
             <Accordion.Header style={header}>
                 <h5>Noite</h5>
-                <Accordion.Trigger style={trigger}>
+                <Accordion.Trigger style={trigger} onClick={afternoon}>
                     <ChevronDownIcon style={chevron} aria-hidden />
                 </Accordion.Trigger>
             </Accordion.Header>
             <Accordion.Content>
-                <Entries />
+                <Entries props={dayMoment}/>
             </Accordion.Content>
         </Accordion.Item>
     </Accordion.Root>
-);
+};
 const root = {
     padding: "10px",
     width: "342px",
