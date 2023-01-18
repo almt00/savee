cae1db2c8188e766cb43f80b41c7cdbb8f359719
= <marianaalves20@ua.pt>
2023-01-18 14:56:20 +0000

    Devil Chart

--- components/elements/Chart.js
+++ components/elements/Chart.js
@@ -5,13 +5,12 @@ import { styled } from '@stitches/react';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import useSWR from 'swr';
 
-
 const data = {
-  labels: ['User1', 'User2'],
+  labels: ['User2', 'User1'],
   datasets: [
     {
       data: [60, 50],
-      backgroundColor: ['rgb(8, 27, 51)', 'rgba(8, 27, 51, 0.1)'],
+      backgroundColor: ['#B0B0B050', '#081B33'],
     },
   ],
 };
@@ -30,24 +29,7 @@ const options = {
   },
 };
 
-//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
-const fetcher = (url) =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => JSON.parse(res));
-
 const DoughnutChart = () => {
-  // const { data_fetch, error } = useSWR('/api/tasks', fetcher);
-
-  // //Handle the error state
-  // if (error) return <div>Failed to load</div>;
-  // //Handle the loading state
-  // if (!data_fetch) return <div>Loading...</div>;
-
-  // if (data_fetch) {
-  //   console.log(data_fetch.hist_payment);
-  // }
-
   return (
     <Container>
       <Doughnut data={data} options={options} plugins={[ChartDataLabels]} />
