2c42073487545382165e6c66be0e4dc7b4731589
= <marianaalves20@ua.pt>
2023-01-20 13:21:35 +0000

    [Design system] Percentagem users Fixes #54

--- components/elements/Chart.js
+++ components/elements/Chart.js
@@ -3,42 +3,72 @@ import Chart from 'chart.js/auto';
 import { Doughnut } from 'react-chartjs-2';
 import { styled } from '@stitches/react';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import useSWR from 'swr';
 
-const data = {
-  labels: ['User2', 'User1'],
-  datasets: [
+const DoughnutChart = props => {
+  let border = '';
+  let background = '';
+  if (props.environment === 'payment') {
+    background = ['#FFFFFF', '#FFFFFF', '#C5E1F2'];
+    border = '#081B33';
+  } else {
+    background = ['#B0B0B050', '#B0B0B050', '#081B33'];
+    border = '#FFFFFF';
+  }
+
+  let totalValue = 55; //não consegui colocar o 55 na linha inferior
+  let ourUserValue = 22.5;
+  let message = ourUserValue + '€';
+  const data = {
+    labels: ['User3', 'User2', 'User1'],
+    datasets: [
+      {
+        data: [20, 25, 55],
+        backgroundColor: background,
+        borderColor: border,
+        borderWidth: 1.5,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        display: true,
+        color: 'white',
+        backgroundColor: 'grey',
+        borderRadius: '100',
+      },
+    },
+  };
+
+  const plugins = [
     {
-      data: [60, 50],
-      backgroundColor: ['#B0B0B050', '#081B33'],
+      beforeDraw: function (chart) {
+        let width = chart.width,
+          height = chart.height,
+          ctx = chart.ctx;
+        ctx.restore();
+        let fontSize = (height / 160).toFixed(2);
+        ctx.font = fontSize + 'em sans-serif';
+        ctx.textBaseline = 'middle';
+        let text = message,
+          textX = Math.round((width - ctx.measureText(text).width) / 2),
+          textY = height / 2;
+        ctx.fillText(text, textX, textY);
+        ctx.save();
+      },
     },
-  ],
-};
-
-const options = {
-  plugins: {
-    legend: {
-      display: false,
-    },
-    datalabels: {
-      display: true,
-      color: 'white',
-      backgroundColor: 'grey',
-      borderRadius: '100',
-    },
-  },
-};
-
-const DoughnutChart = () => {
+  ];
   return (
-    <Container>
-      <Doughnut data={data} options={options} plugins={[ChartDataLabels]} />
-    </Container>
+    <Doughnut
+      data={data}
+      options={options}
+      plugins={props.environment === 'payment' ? plugins : [ChartDataLabels]}
+    />
   );
 };
-
-const Container = styled('div', {
-  width: '30%',
-});
 
 export default DoughnutChart;
---
+++ components/elements/PaymentCard.js
@@ -0,0 +1,86 @@
+import { styled } from '@stitches/react';
+import Chart from './Chart';
+import Button from './Button';
+
+const PaymentCard = () => {
+  let percentageOurUser = 55;
+  let percentageColleagueA = 25;
+  let percentageColleagueB = 20;
+ 
+  return (
+    <div>
+      <Text className='mb-5'>
+        Com base nos teus comportamentos deste mês, este é o valor aproximado a
+        pagares:
+      </Text>
+      <div className='w-56 m-auto'>
+      <Chart className='mt-5' environment='payment'></Chart>
+      </div>
+      <div className='text-center my-4'>
+        <Link href="">Como calculamos este valor?</Link>
+      </div>
+      <UsersContainer>
+        <User>
+          <Img src='/img/user_bird_profile_icon.svg' alt='Avatar' />
+          <SubTitle>Pedro</SubTitle>
+          <Percentage>{percentageOurUser}%</Percentage>
+        </User>
+        <User>
+          <Img src='/img/dog_profile_icon.svg' alt='Avatar' />
+          <SubTitle>Asdrúbal</SubTitle>
+          <Percentage>{percentageColleagueA}%</Percentage>
+        </User>
+        <User>
+          <Img src='/img/bird_profile_icon.svg' alt='Avatar' />
+          <SubTitle>Fernando</SubTitle>
+          <Percentage>{percentageColleagueB}%</Percentage>
+        </User>
+      </UsersContainer>
+      <div className='text-center'>
+        <Button className='mt-6' bg='solid' size='lg'>
+          Confirmar
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const UsersContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  gap: '8px',
+  margin: 'auto',
+});
+
+const User = styled('div', {
+  width: '5rem',
+  textAlign: 'center',
+});
+
+const Img = styled('img', {
+  margin: 'auto',
+});
+
+const Link = styled('a', {
+  fontSize: '$small',
+  fontWeight: '$bold',
+  color: '$links',
+});
+
+const Text = styled('p', {
+  color: '$black',
+});
+
+const SubTitle = styled('p', {
+  color: '$black',
+  fontSize: '$small',
+  fontSize: '$bold',
+});
+
+const Percentage = styled('p', {
+  color: '$black',
+  fontSize: '$small',
+});
+
+export default PaymentCard;
