13d7b646abbdbcab55efe8f3fee1f3eb17d84487
amie <6574688+heyamie@users.noreply.github.com>
2023-01-21 10:28:58 +0000

    Update components/elements/Toaster.js

--- components/elements/Toaster.js
+++ components/elements/Toaster.js
@@ -14,7 +14,8 @@ export default function Toaster (){
     return (
       <Container>
         <BannerText>Valor confirmado com sucesso</BannerText>
-        <Cross onClick={hideToaster}>X</Cross>
+<Cross onClick={hideToaster}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+</Cross>
       </Container>
     );
   }
