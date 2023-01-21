086aea4935de48cb635f6e6d4c49519acd4240d2
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-01-21 15:52:39 +0000

    Diminuir margin no 'Ainda não tens conta' margens login e register Fixes #89 Fixes #90

--- pages/register.js
+++ pages/register.js
@@ -77,9 +77,9 @@ export default function Register() {
       <div className="flex justify-center">
         {step === fieldGroups.length - 1 && (
           <Link href="/homepage">
-          <Button className="mt-6" bg="solid" size="lg">
-            Criar conta
-          </Button>
+            <Button className="mt-6" bg="solid" size="lg">
+              Criar conta
+            </Button>
           </Link>
         )}
         {step < 2 && (
@@ -148,12 +148,13 @@ export default function Register() {
         <h1 className="black">Criar conta</h1>
         <p className="black">Cria a tua conta no Savee e começa a poupar.</p>
       </div>
-
-      <Card>
-        {fieldGroups[step]}
-        {/*todo: disable when form validation is set up*/}
-        <Navigation />
-      </Card>
+      <div className="relative px-6 flex flex-col gap-3 pb-6">
+        <Card>
+          {fieldGroups[step]}
+          {/*todo: disable when form validation is set up*/}
+          <Navigation />
+        </Card>
+      </div>
     </>
   );
 }
