edcdc7b46ea963958a8cccef0042e90e5b81433b
amieUA <6574688+heyamie@users.noreply.github.com>
2023-01-21 09:33:42 +0000

    Desespero total

---
+++ components/dashboard/DashboardCard.js
@@ -0,0 +1,72 @@
+import { styled } from '@stitches/react';
+import Chart from '../elements/Chart';
+import Card from '../elements/Card';
+
+//criar switch ou else if :
+//com tendência a subir em comparação com último mÊs _ seta up,
+//tendencia a descer  _seta down,
+//igual ao do mês passado ou empty state sem seta
+
+const DashboardCard = () => {
+  let kwTotalGroup = 22;
+  let kwTotalUser = 53;
+  let numberDays = 22;
+  let percentage = 1.5;
+
+  return (
+    <Card>
+      <Container>
+        <NumberKw>
+          {kwTotalUser} kW
+          <TrendIcon
+            src='/img/tendencia_up.svg'
+            className='ml-6'
+            alt='Pagamentos'
+          />
+          <SubTitle>
+            de <span className='font-bold'>{kwTotalGroup} kW </span>consumidos
+            este mês
+          </SubTitle>
+        </NumberKw>
+        <div className='w-24'>
+          <Chart></Chart>
+        </div>
+      </Container>
+      <Stats className='mb-2'>
+        <span className='font-bold'>{numberDays} dias </span>até ao próximo
+        pagamento
+      </Stats>
+      <Stats>
+        <span className='font-bold'>{percentage}% </span>melhor que o mês
+        passado
+      </Stats>
+    </Card>
+  );
+};
+
+const Container = styled('div', {
+  display: 'flex',
+});
+
+const Stats = styled('p', {
+  color: '$black',
+});
+
+const SubTitle = styled('p', {
+  color: '$black',
+  fontSize: '$small',
+});
+
+const NumberKw = styled('div', {
+  color: '$black',
+  fontSize: '$f0',
+  fontWeight: '$bolder',
+  lineHeight: '3rem',
+  flex: '1',
+});
+
+const TrendIcon = styled('img', {
+  display: 'inline',
+});
+
+export default DashboardCard;
