71d05c5e2717712da62ff3bd69d0803160d1fc24
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-01-22 14:18:31 +0000

    Adicionar tempos de uso/tarefas Fixes #95

--- components/dashboard/Entries.js
+++ components/dashboard/Entries.js
@@ -1,47 +1,94 @@
 import { styled } from "@stitches/react";
-import useSWR from "swr";
-
-//Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
-const fetcher = (url) =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => JSON.parse(res));
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAsyncUser, getUser } from "../../store/UserSlice";
+import { fetchAsyncTasks, getTasks } from "../../store/TasksSlice";
 
 const Entries = () => {
-  //Set up SWR to run the fetcher function when calling api
-  const { data, error } = useSWR("/api/tasks", fetcher);
+  const dispatch = useDispatch();
+  const userData = useSelector(getUser);
+  const tasksData = useSelector(getTasks);
+  let userId = 1;
+  let showEntrylist;
+  let timeUnit = "min";
+  let hours = 0;
+  let min = 0;
 
-  //Handle the error state
-  if (error) return <div>Failed to load</div>;
-  //Handle the loading state
-  if (!data) return <div>Loading...</div>;
-  //console.log(data.tasks);
-
-  const showEntrylist = data.tasks.map((task, index) => {
-    if (index === data.tasks.length-1) {
-      return (
-        <EntryContainer key={task.id} className="border-b-0 mb-0">
-          <EntryImage src={`${task.image}`}></EntryImage>
-          <EntryTitle>{task.name}</EntryTitle>
-          <Minute>
-            <h3>{Math.floor(task.default_time / 60000)}</h3>
-            <p className="ml-1">min</p>
-          </Minute>
-        </EntryContainer>
-      );
-    } else {
-      return (
-        <EntryContainer key={task.id}>
-          <EntryImage src={`${task.image}`}></EntryImage>
-          <EntryTitle>{task.name}</EntryTitle>
-          <Minute>
-            <h3>{Math.floor(task.default_time / 60000)}</h3>
-            <p className="ml-1">min</p>
-          </Minute>
-        </EntryContainer>
-      );
+  useEffect(() => {
+    if (tasksData.status !== 200) {
+      dispatch(fetchAsyncTasks()); // fazer o fetch com redux caso ainda n esteja o estado (ex.: reloads de pagina)
     }
-  });
+    if (userData.status !== 200) {
+      dispatch(fetchAsyncUser(userId)); // fazer o fetch com redux caso ainda n esteja o estado (ex.: reloads de pagina)
+    }
+  }, [dispatch]);
+
+  if (userData.status === 200 && tasksData.status === 200) {
+    let userConsumeHist = userData.user.hist_use;
+    showEntrylist = userConsumeHist.map((element, index) => {
+      let taskId = element.task_id;
+      let chosenTask = tasksData.tasks.find((task) => task.id === taskId);
+      let startTime = new Date(element.start_date);
+      let endTime = new Date(element.end_date);
+      let duration = Math.round(
+        (endTime.getTime() - startTime.getTime()) / 1000 / 60
+      ); // duraçao em min
+      if (duration >= 60) {
+        min = duration % 60;
+        hours = Math.floor(duration / 60);
+        timeUnit = `${hours} h ${min} min`;
+      } else {
+        min = duration;
+        hours = 0;
+        timeUnit = `${min} min`;
+      }
+      if (index === userConsumeHist.length - 1) {
+        return (
+          <EntryContainer key={element.id} className="border-b-0 mb-0">
+            <EntryImage src={`${chosenTask.image}`}></EntryImage>
+            <EntryTitle>{element.name}</EntryTitle>
+            <Minute>
+            {duration >= 60 ? (
+                <>
+                  <h3>{hours} </h3>
+                  <p className="ml-1">h </p>
+                  <h3 className="ml-1">{min} </h3>
+                  <p className="ml-1"> min</p>
+                </>
+              ) : (
+                <>
+                  <h3>{min}</h3>
+                  <p className="ml-1">min</p>
+                </>
+              )}
+            </Minute>
+          </EntryContainer>
+        );
+      } else {
+        return (
+          <EntryContainer key={element.id}>
+            <EntryImage src={`${chosenTask.image}`}></EntryImage>
+            <EntryTitle>{element.name}</EntryTitle>
+            <Minute>
+              {duration >= 60 ? (
+                <>
+                  <h3>{hours} </h3>
+                  <p className="ml-1"> h </p>
+                  <h3 className="ml-1">{min} </h3>
+                  <p className="ml-1"> min</p>
+                </>
+              ) : (
+                <>
+                  <h3>{min}</h3>
+                  <p className="ml-1">min</p>
+                </>
+              )}
+            </Minute>
+          </EntryContainer>
+        );
+      }
+    });
+  }
 
   return <div>{showEntrylist}</div>;
 };
