46d24e84ccabd18ea8a2633d0ec201a7394f3e61
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-01-23 12:52:47 +0000

    try fix banner

--- components/elements/Banner.js
+++ components/elements/Banner.js
@@ -1,75 +1,73 @@
-import { styled } from '@stitches/react';
-import Button from './Button';
+import { styled } from "@stitches/react";
+import Button from "./Button";
 import Link from "next/link";
 
 import React from "react";
-import { fetchAsyncGroup, getGroup } from '../../store/GroupSlice';
+import { fetchAsyncGroup, getGroup } from "../../store/GroupSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
-
-export default function Banner(props) {//via props é enviado o dia de hoje
-
-
+export default function Banner(props) {
+  //via props é enviado o dia de hoje
   const [isShown, setIsShown] = useState(true);
-
-  function ShownFalse() {
-    setIsShown(false)
-  }
-
-  let toDay = props.toDay;//recolhe do props o valor do dia atual para depois comparar
-
-  const id = 1;//Id para identificar o grupo a ir buscar dados
-  let invoice = "";//para guardar data do grupo
-
   const dispatch = useDispatch();
   const groupData = useSelector(getGroup);
 
   useEffect(() => {
-    dispatch(fetchAsyncGroup(id)); // fazer o fetch com redux
+    if (groupData.status !== 200) {
+      dispatch(fetchAsyncGroup(id)); // fazer o fetch com redux
+    }
   }, [dispatch]);
 
-  if (groupData.status === 200) {
-    invoice = groupData.group.invoice_date;//Guarda o dia do invoice do grupo
-    console.log(invoice)
+  function ShownFalse() {
+    setIsShown(false);
   }
-  const groupFullDate = new Date(invoice);
-  const payDate = groupFullDate.getDate();
-  console.log(payDate);
 
-  const checkDate = toDay + 4;
-  if (payDate <= checkDate) {
-    console.log("inside if")
-    //setIsShown(true) não me permite alterar a variável de estado, "react limita o número de renders para evitar loops"
+  let toDay = props.toDay; //recolhe do props o valor do dia atual para depois comparar
+  const id = 1; //Id para identificar o grupo a ir buscar dados
+  let invoice = ""; //para guardar data do grupo
+  let payDate;
+  let checkDate;
+
+  if (groupData.status === 200) {
+    invoice = groupData.group.invoice_date; //Guarda o dia do invoice do grupo
+    const groupFullDate = new Date(invoice);
+    payDate = groupFullDate.getDate();
+    checkDate = payDate - 2;
+
+    if (toDay >= checkDate && isShown === false) {
+      setIsShown(true); //não me permite alterar a variável de estado, "react limita o número de renders para evitar loops"
+    } else if (toDay < checkDate && isShown === true) {
+      setIsShown(false);
+    }
   }
+
   //Se os dois valores forem iguais o banner é apresentado
-  if ((payDate <= checkDate) & (isShown == true)) {
-    console.log("sucess")
+  if (isShown === true && toDay >= checkDate) {
     return (
       <Container>
         <BannerText>Está na hora de pagar!</BannerText>
-        <Link href='/invoice'>
-          <Button size='md' bg='primary' onClick={ShownFalse}>
+        <Link href="/invoice">
+          <Button size="md" bg="primary" onClick={ShownFalse}>
             Inserir fatura
           </Button>
         </Link>
       </Container>
     );
   }
-};
+}
 
-const BannerText = styled('p', {
-  color: '$white',
-  fontSize: '$normal',
-  fontWeight: '$normal',
-  alignSelf: 'center',
+const BannerText = styled("p", {
+  color: "$white",
+  fontSize: "$normal",
+  fontWeight: "$normal",
+  alignSelf: "center",
 });
 
-const Container = styled('div', {
-  backgroundColor: '$black',
-  borderRadius: '12px',
-  padding: '1rem 1.5rem',
-  display: 'flex',
-  justifyContent: 'space-between',
+const Container = styled("div", {
+  backgroundColor: "$black",
+  borderRadius: "12px",
+  padding: "1rem 1.5rem",
+  display: "flex",
+  justifyContent: "space-between",
 });
-
