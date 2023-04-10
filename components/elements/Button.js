bee302d0b9d9c3ab87f6a5f4d683b7440c3be03f
= <marianaalves20@ua.pt>
2023-04-10 09:16:32 +0100

    simple_example_buttons

--- components/elements/Button.js
+++ components/elements/Button.js
@@ -1,56 +1,182 @@
 import { styled } from '@stitches/react';
-import PropTypes from 'prop-types';
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
-  return (
-    <button
-      type='button'
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
-      )}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+export const Button = styled('button', {
+  borderRadius: '12px',
+  backgroundColor: '$white',
+  color: '$black',
+  fontSize: '$small',
+  fontWeight: '$bold',
+  padding: '0.4rem 0.75rem',
+  border: '1px solid $black',
+  '&:focus-visible': {
+    border: '2px solid $links',
+  },
+  '&:hover:enabled': {
+    backgroundColor: '$border',
+  },
+  '&:disabled': {
+    border: '1px solid $muted',
+    color: '$muted',
+  },
 
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
+  variants: {
+    size: {
+      md: {
+        padding: '0.4rem 0.75rem',
+      },
+      lg: {
+        fontSize: '$normal',
+        fontWeight: '$bolder',
+        padding: '0.6rem 1rem',
+      },
+      sm: {
+        padding: '0.3rem 0.5rem',
+      },
+    },
+    bg: {
+      primary: {
+        backgroundColor: '$white',
+        border: '1px solid $black',
+        color: '$black',
+      },
+      secondary: {
+        backgroundColor: 'transparent',
+        border: '1px solid $border',
+        color: '$links',
+        '&:focus-visible': {
+          border: '2px solid $links',
+          backgroundColor: 'transparent',
+        },
+        '&:hover:enabled': {
+          color: '$white',
+          backgroundColor: '$links',
+        },
+        '&:disabled': {
+          color: '$links',
+          opacity: 0.5,
+          border: '1px solid $border',
+        },
+      },
+      transparent: {
+        backgroundColor: 'transparent',
+        border: '0px',
+        color: '$links',
+        '&:disabled': {
+          border: 'none',
+          backgroundColor: '$white',
+          color: '$muted',
+        },
+      },
+      solid: {
+        backgroundColor: '$success',
+        border: '0px',
+        color: '$white',
+        '&[type=submit]': {
+          backgroundColor: '$success',
+          border: '0px',
+          color: '$white',
+        },
+        '&:hover:enabled': {
+          backgroundColor: '#2A824E',
+        },
+        '&:disabled': {
+          backgroundColor: '$success',
+          opacity: 0.5,
+          color: '$white',
+          border: 'none',
+        },
+      },
+      danger: {
+        backgroundColor: '$danger',
+        border: '0px',
+        color: '$white',
+        '&:hover:enabled': {
+          backgroundColor: '#AB2A2A',
+        },
+        '&:disabled': {
+          backgroundColor: '$danger',
+          opacity: 0.5,
+          color: '$white',
+          border: 'none',
+        },
+      },
+    },
+  },
+});
 
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
-};
+export const PrimaryButton = styled(Button, {
+  backgroundColor: '$white',
+  border: '1px solid $black',
+  color: '$black',
+});
+
+export const SecondaryButton = styled(Button, {
+  backgroundColor: 'transparent',
+  border: '1px solid $border',
+  color: '$links',
+});
+
+export const SuccessButton = styled(Button, {
+  backgroundColor: '$success',
+  border: '0px',
+  color: '$white',
+});
+
+export const LargeButton = styled(Button, {
+  backgroundColor: 'purple',
+  fontSize: '24px',
+});
+
+// /**
+//  * Primary UI component for user interaction
+//  */
+// export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+//   const mode = primary
+//     ? 'storybook-button--primary'
+//     : 'storybook-button--secondary';
+//   return (
+//     <button
+//       type='button'
+//       className={['storybook-button', `storybook-button--${size}`, mode].join(
+//         ' '
+//       )}
+//       style={backgroundColor && { backgroundColor }}
+//       {...props}
+//     >
+//       {label}
+//     </button>
+//   );
+// };
+
+// Button.propTypes = {
+//   /**
+//    * Is this the principal call to action on the page?
+//    */
+//   primary: PropTypes.bool,
+//   /**
+//    * What background color to use
+//    */
+//   backgroundColor: PropTypes.string,
+//   /**
+//    * How large should the button be?
+//    */
+//   size: PropTypes.oneOf(['small', 'medium', 'large']),
+//   /**
+//    * Button contents
+//    */
+//   label: PropTypes.string.isRequired,
+//   /**
+//    * Optional click handler
+//    */
+//   onClick: PropTypes.func,
+// };
+
+// Button.defaultProps = {
+//   backgroundColor: null,
+//   primary: false,
+//   size: 'medium',
+//   onClick: undefined,
+// };
 
 // const Button = styled("button", {
 //   // default: medium primary
--- stories/Button.stories.jsx
+++ stories/Button.stories.jsx
@@ -1,40 +1,67 @@
-import React from 'react';
+import { Button, PrimaryButton, SecondaryButton, SuccessButton, LargeButton } from '../components/elements/Button';
 
-import { Button } from '../components/elements/Button';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: "Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = args => <Button {...args} />;
+export const Primary = () => <PrimaryButton>Botão!</PrimaryButton>;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const Secondary = () => <SecondaryButton>Click me!</SecondaryButton>;
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+export const Success = () => <SuccessButton>Click me!</SuccessButton>;
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// import React from 'react';
+// import { Button } from '../components/elements/Button';
+
+
+// export default {
+//   title: 'Button',
+//   component: Button,
+// };
+
+// export const Default = () => <Button>Click me!</Button>;
+
+// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// export default {
+//   title: 'Example/Button',
+//   component: Button,
+//   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+// };
+
+// // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// const Template = args => <Button {...args} />;
+
+// export const Primary = Template.bind({});
+// // More on args: https://storybook.js.org/docs/react/writing-stories/args
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
+
+
+// import { storiesOf } from '@storybook/react';
+// import { Button } from '../components/Button';
+
+// storiesOf('Button', module).add('Default', () => <Button />);
