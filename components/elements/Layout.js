0b36edf8d3134d5b4f69edd677a57ea436534fb6
= <marianaalves20@ua.pt>
2023-04-19 23:27:54 +0100

    duplicated main homepage

--- components/elements/Layout.js
+++ components/elements/Layout.js
@@ -1,19 +1,19 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 const Layout = ({
   children,
-  title = "default title",
-  description = "default description",
+  title = 'default title',
+  description = 'default description',
 }) => {
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content={description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>{children}</main>
+      <>{children}</>
     </>
   );
 };
