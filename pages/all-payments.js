5087427ef6eba06c9a7390fe6e37b995f4e21554
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-04-22 14:11:51 +0100

    fixed functions and state

--- pages/all-payments.js
+++ pages/all-payments.js
@@ -8,52 +8,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { setPage } from "../store/PageSlice";
 import { fetchAsyncPaymentSlice, getPayment } from "../store/PaymentSlice";
-import {
-  fetchAsyncPaymentGroupSlice,
-  getPaymentGroup,
-} from "../store/PaymentGroupSlice";
-import {
-  fetchAsyncPaymentGroupDetailsSlice,
-  getPaymentGroupDetails,
-} from "../store/PaymentGroupDetailsSlice";
 
 const AllPayments = () => {
   const dispatch = useDispatch();
   const paymentData = useSelector(getPayment);
-  const paymentGroupData = useSelector(getPaymentGroup);
-  const paymentGroupDetailsData = useSelector(getPaymentGroupDetails);
-  dispatch(setPage("payments"));
 
   const userId = 1;
-  const houseId = 1;
-  const paymentId = 1;
-  let date = "";
-  let cleanDate = "";
-  let userValue = "";
-  let obj = "";
-  let objGroup = "";
-  let PayHisto = "";
-  let lastPayment = "";
-  let lastDate;
+  const obj = paymentData.payment;
+
   useEffect(() => {
-    if (
-      paymentData.status !== 200 &&
-      paymentGroupData.status !== 200 &&
-      paymentGroupDetailsData.status !== 200
-    ) {
+    dispatch(setPage("payments"));
+    if (paymentData.status !== 200) {
       dispatch(fetchAsyncPaymentSlice(userId)); // fazer o fetch com redux caso ainda n esteja o estado (ex.: reloads de pagina)
-      dispatch(fetchAsyncPaymentGroupSlice(houseId));
-      dispatch(fetchAsyncPaymentGroupDetailsSlice(houseId, paymentId));
     }
   }, [dispatch]);
 
-  if (paymentData.status === 200) {
-    obj = paymentData.payment;
-
-    lastPayment = () => {
+  const lastPayment = () => {
+    if (paymentData.status === 200) {
       let lastPaymentData = obj[0]?.payment;
       const options = { month: "long", day: "numeric" };
-      lastDate = new Date(lastPaymentData.date_payment).toLocaleDateString(
+      let lastDate = new Date(lastPaymentData.date_payment).toLocaleDateString(
         "pt-PT",
         options
       );
@@ -63,35 +37,38 @@ const AllPayments = () => {
           <p className="mt-2">Pagos a {lastDate}</p>
         </Card>
       );
-    };
-    
-    PayHisto = obj?.map((payment, index) => {
-      totalValue = payment.payment.value_payment;
-      value = payment.payment_percentage * totalValue;
-      date = payment.payment.date_payment;
-      const options = { month: "short", day: "numeric" };
-      cleanDate = new Date(date).toLocaleDateString("pt-PT", options);
+    }
+  };
+  let PayHisto = () => {
+    if (paymentData.status === 200) {
+      return obj?.map((payment, index) => {
+        let totalValue = payment.payment.value_payment;
+        let value = payment.payment_percentage * totalValue;
+        let date = payment.payment.date_payment;
+        const options = { month: "short", day: "numeric" };
+        let cleanDate = new Date(date).toLocaleDateString("pt-PT", options);
 
-      return (
-        <>
-          <Link href={`/payment?id=${payment.payment_id}`}>
-            <Card type="stroke" key={index}>
-              <CardItem
-                className="flex justify-between items-center"
-                key={index}
-              >
-                <PaymentInfo key={index}>
-                  <h4>{value}€</h4>
-                  <p className="mt-1">de {totalValue}€ totais</p>
-                </PaymentInfo>
-                <p className="text-muted">{cleanDate}</p>
-              </CardItem>
-            </Card>
-          </Link>
-        </>
-      );
-    });
-  }
+        return (
+          <li key={index}>
+            <Link href={`/payment?id=${payment.payment_id}`}>
+              <Card type="stroke">
+                <CardItem
+                  className="flex justify-between items-center"
+                  key={index}
+                >
+                  <PaymentInfo>
+                    <h4>{value}€</h4>
+                    <p className="mt-1">de {totalValue}€ totais</p>
+                  </PaymentInfo>
+                  <p className="text-muted">{cleanDate}</p>
+                </CardItem>
+              </Card>
+            </Link>
+          </li>
+        );
+      });
+    }
+  };
 
   return (
     <Layout title="Histórico pagamentos" description="Histórico pagamentos">
@@ -100,7 +77,7 @@ const AllPayments = () => {
       <div className="relative pt-20 px-6 flex flex-col gap-3 pb-6">
         {lastPayment()}
         <h3 className="mt-6">Histórico de pagamento</h3>
-        {PayHisto}
+        <ul>{PayHisto()}</ul>
       </div>
     </Layout>
   );
