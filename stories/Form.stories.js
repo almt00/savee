92999434285560092db8fc075679da0d8c82d69f
= <marianaalves20@ua.pt>
2023-05-02 08:43:51 +0100

    placeholder

--- stories/Form.stories.js
+++ stories/Form.stories.js
@@ -1,24 +1,22 @@
 import { Input_storybook } from '../components/elements/Form';
 
-
 export default {
   title: 'Input',
   component: Input_storybook,
   argTypes: {
-   
+    color: { control: { type: 'color', presetColors: ['red', 'green'] } },
+    required: { control: 'boolean' }
   },
 };
 
 const Template = args => <Input_storybook {...args} />;
 
-export const Default= Template.bind({});
+export const Default = Template.bind({});
 Default.args = {
   backgroundColor: 'white',
-  color: "black",
-  border: "1px solid",
-  label:"Simple field",
-  placeholder:"Batata"
+  color: 'black',
+  border: '1px solid',
+  label: 'Simple field',
+  placeholder: 'Batata',
+  required: '*'
 };
-
-
-
