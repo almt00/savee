ed405df95f94a54f4ff9ed9fa7cc4855f75690b7
amieUA <6574688+heyamie@users.noreply.github.com>
2023-05-03 18:38:57 +0100

    Add session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-import '../styles/globals.css';
-import { Inter } from '@next/font/google';
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
+import "../styles/globals.css";
+import { Inter } from "@next/font/google";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { SessionProvider, sessionProvider } from "next-auth/react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
@@ -24,34 +25,47 @@ function useWindowSize() {
       });
     }
     // Add event listener
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Call handler right away so state gets updated with initial window size
     handleResize();
     // Remove event listener on cleanup
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []); // Empty array ensures that effect is only run on mount
   return windowSize;
 }
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   const size = useWindowSize();
   const width = size.width;
 
   return (
-    <Provider Provider store={store}>
-      <main className={inter.className}>
-        {width > 450 ? (
-          <div className='h-screen bg-gray-50 flex items-center justify-center p-5'>
-            <Image src='/img/logo.svg' className='mr-5' alt='Savee logo'width="77" height="34" />
-            <h3 className=''>Esta plataforma só está preparada para mobile.</h3>
-          </div>
-        ) : (
-          <>
-            <Component {...pageProps} />
-          </>
-        )}
-      </main>
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider Provider store={store}>
+        <main className={inter.className}>
+          {width > 450 ? (
+            <div className="h-screen bg-gray-50 flex items-center justify-center p-5">
+              <Image
+                src="/img/logo.svg"
+                className="mr-5"
+                alt="Savee logo"
+                width="77"
+                height="34"
+              />
+              <h3 className="">
+                Esta plataforma só está preparada para mobile.
+              </h3>
+            </div>
+          ) : (
+            <>
+              <Component {...pageProps} />
+            </>
+          )}
+        </main>
+      </Provider>
+    </SessionProvider>
   );
 }
