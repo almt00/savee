10b8a542d977ed04b513fa3dad1b9ea70aef5126
= <marianaalves20@ua.pt>
2023-05-04 09:45:50 +0100

    chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
 module.exports = {
-  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
-  "framework": {
-    name: "@storybook/nextjs",
-    options: {}
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
