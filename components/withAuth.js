bb3da77346ca07b5b747c453ab2ab1c51e3c9694
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-06-17 14:11:44 +0100

    fixed flashing screen

--- components/withAuth.js
+++ components/withAuth.js
@@ -1,21 +1,23 @@
 // components/RequireAuth.js
 import Cookies from "js-cookie";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function withAuth(Component) {
   return function ProtectedRoute({ ...props }) {
+    const [isLoading, setIsLoading] = useState(true);
     const router = useRouter();
     const user = Cookies.get("userToken");
-    console.log(user);
     const userIsAuthenticated = user !== undefined;
 
     useEffect(() => {
       if (!userIsAuthenticated) {
         router.push("/login");
+      } else {
+        setIsLoading(false);
       }
     }, [userIsAuthenticated, router]);
 
-    return <Component {...props} />;
+    return <>{isLoading ? <div></div> : <Component {...props} />}</>;
   };
 }
